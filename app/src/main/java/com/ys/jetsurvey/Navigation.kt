299b43f3e5c7273d48e286ef1cd613d4ef1e65b1
YS <yoonsam86@gmail.com>
2022-10-27 09:46:38 +0900

    Add Navigation

---
+++ app/src/main/java/com/ys/jetsurvey/Navigation.kt
@@ -0,0 +1,31 @@
+package com.ys.jetsurvey
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import java.security.InvalidParameterException
+
+enum class Screen { Welcome, SignUp, SignIn, Survey }
+
+fun Fragment.navigate(to: Screen, from: Screen) {
+    if (to == from) {
+        throw InvalidParameterException("Can't navigate to $to")
+    }
+
+    when (to) {
+        Screen.Welcome -> {
+            findNavController().navigate(R.id.welcome_fragment)
+        }
+
+        Screen.SignUp -> {
+
+        }
+
+        Screen.SignIn -> {
+
+        }
+
+        Screen.Survey -> {
+
+        }
+    }
+}
